f1d32994922e16b19e4ba01c83789e423ec0bca9
Rhesmika <Rhesmika@gmail.com>
2023-02-05 15:15:30 +0000

    add post function to tasks to create tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,21 +1,37 @@
-from django.http import Http404
-from rest_framework.views import APIView
+from rest_framework import status, permissions
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Task
 from .serializers import TaskSerializer
-from tudu_api.permissions import IsOwnerOrReadOnly
 
 
 class TaskList(APIView):
+    serializer_class = TaskSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
     def get(self, request):
         tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True, context={'request': request})
+        serializer = TaskSerializer(
+            tasks, many=True, context={'request': request}
+        )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class TaskDetail(APIView):
-    serializer_class = TaskSerializer
-    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
@@ -29,5 +45,3 @@ class TaskDetail(APIView):
         task = self.get_object(pk)
         serializer = TaskSerializer(task, context={'request': request})
         return Response(serializer.data)
-
-
