3f83fdab6302702bec99dd7532acb73daf74fcbb
Rhesmika <Rhesmika@gmail.com>
2023-02-20 10:03:18 +0000

    add member_id to the teams view

--- teams/serializers.py
+++ teams/serializers.py
@@ -1,23 +1,49 @@
 from rest_framework import serializers
 from teams.models import Team
+from members.models import Member
 
 
 class TeamSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    member_id = serializers.SerializerMethodField()
+    # def get_following_id(self, obj):
+    #     user = self.context['request'].user
+    #     if user.is_authenticated:
+    #         following = Follower.objects.filter(
+    #             owner=user, followed=obj.owner
+    #         ).first()
+    #         # print(following)
+    #         return following.id if following else None
+    #     return None
+
+
+    def get_member_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            member = Member.objects.filter(
+                owner=user, team=obj.id
+            ).first()
+            # print(following)
+            return member.id if member else None
+        return None
+
+
+
     # members = serializers.SlugRelatedField(
     #     many=True,
     #     read_only=True,
     #     slug_field='username'
     # )
 
-
     # is_member = serializers.SerializerMethodField()
-    is_owner = serializers.SerializerMethodField()
 
     # def get_is_member(self, obj):
     #     request = self.context['request']
     #     return obj.members.filter(username=request.user).exists()
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -26,5 +52,5 @@ class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = [
-            'id', 'created_at', 'name', 'description', 'image', 'owner', 'is_owner',
+            'id', 'created_at', 'name', 'description', 'image', 'owner', 'is_owner','member_id'
         ]
