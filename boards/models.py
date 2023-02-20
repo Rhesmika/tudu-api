6c1b9953c84e0159a02a1c017aef44af7b926b64
Rhesmika <Rhesmika@gmail.com>
2023-02-20 19:57:16 +0000

    remove owner from boards app

--- boards/models.py
+++ boards/models.py
@@ -7,7 +7,6 @@ from teams.models import Team
 class Board(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
     class Meta:
--- boards/serializers.py
+++ boards/serializers.py
@@ -3,22 +3,14 @@ from boards.models import Board
 
 
 class BoardSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source="owner.username")
-    is_owner = serializers.SerializerMethodField()
     task_count = serializers.ReadOnlyField()
-
-    def get_is_owner(self, obj):
-        request = self.context["request"]
-        return request.user == obj.owner
 
     class Meta:
         model = Board
         fields = [
             "id",
-            "owner",
             "created_at",
             "name",
-            "is_owner",
             "team",
             "task_count",
         ]
