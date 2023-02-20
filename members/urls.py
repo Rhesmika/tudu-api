20b580b083dc34ff6c9731b77745c06e3c009922
Rhesmika <Rhesmika@gmail.com>
2023-02-20 09:13:21 +0000

    create member detail view

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,6 @@ from members import views
 
 urlpatterns = [
     path('members/', views.MemberList.as_view()),
+    path('members/<int:pk>/', views.MemberDetail.as_view()),
+
 ]
--- members/views.py
+++ members/views.py
@@ -1,16 +1,35 @@
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Member
 from .serializers import MemberSerializer
+from tudu_api.permissions import IsOwnerOrReadOnly, IsAuthenticated
 
 
-class MemberList(APIView):
+class MemberList(generics.ListCreateAPIView):
+    serializer_class = MemberSerializer
+    queryset = Member.objects.all()
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+        return Response(serializer.data)
+
+class MemberDetail(APIView):
+
+    permission_classes = [IsAuthenticated]
     serializer_class = MemberSerializer
 
-    def get(self, request):
-        members = Member.objects.all()
-        serializer = MemberSerializer(
-            members, many=True, context={'request': request}
-        )
-        
+    def get_object(self, pk):
+        try:
+            member = Member.objects.get(pk=pk)
+            self.check_object_permissions(self.request, member)
+            return member
+        except Member.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        member = self.get_object(pk)
+        serializer = MemberSerializer(member, context={'request': request})
         return Response(serializer.data)
