df3dc056b84957dd52aa9101bb8a7f1db9424886
Rhesmika <Rhesmika@gmail.com>
2023-05-14 16:09:50 +0000

    redirect default database

--- tudu_api/settings.py
+++ tudu_api/settings.py
@@ -66,7 +66,8 @@ ALLOWED_HOSTS = [
     os.environ.get("ALLOWED_HOST"),
     "localhost",
 ]
-CSRF_TRUSTED_ORIGINS = ["https://8000-rhesmika-tuduapi-wzmbjhfp100.ws-eu92.gitpod.io"]
+CSRF_TRUSTED_ORIGINS = ["https://8000-rhesmika-tuduapi-wzmbjhfp100.ws-us97.gitpod.io"]
+
 
 
 # Application definition
@@ -145,15 +146,15 @@ WSGI_APPLICATION = "tudu_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if "DEV" in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+# if "DEV" in os.environ:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
+# else:
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
