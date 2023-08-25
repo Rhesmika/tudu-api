6854687484cdf8bff815167525982e9c7a3afd71
Rhesmika <Rhesmika@gmail.com>
2023-08-25 13:04:59 +0000

    adjust task serliazer to collect board.id instead of board name

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from tasks.models import Task
 from django.contrib.auth.models import User
+from boards.models import Board
 
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
-    board = serializers.ReadOnlyField(source="board.name")
+    board = serializers.PrimaryKeyRelatedField(queryset=Board.objects.all())
 
     def get_is_owner(self, obj):
         request = self.context["request"]
